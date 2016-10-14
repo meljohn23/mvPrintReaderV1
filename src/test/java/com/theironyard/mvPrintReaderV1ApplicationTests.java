9495e3de7d4bedbcea850e86fcfa99ba7646cc47
Melanie Johnson <meljohn23@msn.com>
2016-10-14 11:08:54 -0400

    got it like I want 10-14-16_AM

--- src/test/java/com/theironyard/mvPrintReaderV1ApplicationTests.java
+++ src/test/java/com/theironyard/mvPrintReaderV1ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RssReaderDemoApplicationTests {
+public class mvPrintReaderV1ApplicationTests {
 
 	@Test
 	public void contextLoads() {
